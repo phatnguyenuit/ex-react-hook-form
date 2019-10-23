e8e8793ecf428f7b96a46e88e5af2dfc1908a0e1
Nguyen Phat <phatnt.uit@gmail.com>
2019-10-23 22:44:14 +0700

    Add form

--- src/components/RegisterForm/index.tsx
+++ src/components/RegisterForm/index.tsx
@@ -1,10 +1,23 @@
-import React, { FC, memo } from 'react';
+import React, { memo } from 'react';
+import useForm from 'react-hook-form';
+import { FieldValues, OnSubmit } from 'react-hook-form/dist/types';
 
-const RegisterFormComponent: FC = () => {
+const RegisterFormComponent = () => {
+  const { register, handleSubmit, watch, errors } = useForm();
+  const onSubmit: OnSubmit<FieldValues> = (data) => { console.log(data); };
+  console.log(watch('example'));
   return (
-    <div>
-      ahihi
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>Example</label>
+      <input name='example' defaultValue='test' ref={register} />
+      <label>ExampleRequired</label>
+      <input
+        name="exampleRequired"
+        ref={register({ required: true, maxLength: 10 })}
+      />
+      {errors.exampleRequired && <p>This field is required</p>}
+      <input type='submit' />
+    </form>
   );
 };
 const RegisterForm = memo(RegisterFormComponent);
